61363cb9a6632e2adb0419ff45c1d4a53edcf797
pkb32 <113055313+pkb32@users.noreply.github.com>
2025-02-05 20:34:01 +0530

    images setting updated

--- src/components/characterSection/HeroPic.jsx
+++ src/components/characterSection/HeroPic.jsx
@@ -12,7 +12,7 @@ function HeroPic() {
       viewport={{ once: false, amount: 0 }}
     className='h-full flex items-center justify-center'>
         
-    <img src="./../public/images/Profile2.png" alt=""  className='max-h-[450px] w-auto'/>
+    <img src="/images/Profile2.png" alt=""  className='max-h-[450px] w-auto'/>
       <div className=" absolute -z-10 flex justify-center items-center animate-pulse ">
         <PiStarOfDavidLight  className=" md:h-[90%] sm:h-[120%] min-h-[600px] w-auto text-cyan blur-md animate-[spin_20s_linear_infinite] " />
       </div>
--- src/components/experienceSection/ExperienceTopMiddle.jsx
+++ src/components/experienceSection/ExperienceTopMiddle.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function ExperienceTopMiddle() {
   return (
     <div className="lg:w-[35%] md:w-[50%] sm:w-[80%]">
-    <img src="../../public/images/experience-image.png" alt="my experience" />
+    <img src="/images/experience-image.png" alt="my experience" />
   </div>
   )
 }
